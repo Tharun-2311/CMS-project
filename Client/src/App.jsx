3b52eb8d1b818d6690af028aef4a3aad0770eabb
sriram-sri-hub <sriram451803@gmail.com>
2025-05-30 16:28:38 +0530

    signin page

--- Client/src/App.jsx
+++ Client/src/App.jsx
@@ -3,14 +3,16 @@ import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
 import Home from './pages/Home'
+import Signin from './pages/Signin'
 
 function App() {
   const [count, setCount] = useState(0)
 
   return (
     <>
-      <Home/>
-    </>
+     <Signin/>
+      </>
+   
   )
 }
 
